cc7bbe2c1806cb5280566c212ae6b1b90dfbdff3
brand7999 <brand7999@gmail.com>
2025-10-05 11:23:53 +0700

    move hello world to function

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-print('Hello World')
-print('My Name is vatin')
-print('Hello World')
-print('My Name is vatin')
+## A simple Python script that prints "Hello World" and a name.
+def hellworld():
+    print('Hello World')
+    print('My Name is vatin')
 
+hellworld()
